f377d80535a2827fac1f014c565c25f1cb902b5c
Kertész Gábor <gaborkertesz@hotmail.com>
2019-11-07 03:05:22 +0100

    desing layout done

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function ChangeBcolor()
+function ToogleDarkMode() 
 {
-    document.body.style.backgroundColor = "red";
+    var docBody = document.getElementById("body");
+    var getBodiesCurrentClass = docBody.className;
+    var buttonvar = document.getElementById("btn");
+    if (getBodiesCurrentClass == "light-mode")
+        {
+            docBody.className = "dark-mode";
+            document.getElementById("content").style.color= '#353942'
+            buttonvar.innerHTML = "Light Mode";
+            buttonvar.className = "btn text-center btn-light";
+        }
+    else
+        {
+            docBody.className = "light-mode";
+            buttonvar.innerHTML = "Dark Mode";
+            buttonvar.className = "btn text-center btn-dark";
+        }
 }
