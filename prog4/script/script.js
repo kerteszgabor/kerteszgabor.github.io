8e2ef9e4566a71b64687f2ec38aad2a32fa24f45
Kertész Gábor <gaborkertesz@hotmail.com>
2020-02-29 23:19:07 +0100

    created prog4 main site

---
+++ prog4/script/script.js
@@ -0,0 +1,23 @@
+function ToogleDarkMode() 
+{
+    var docBody = document.getElementById("body");
+    var getBodiesCurrentClass = docBody.className;
+    var buttonvar = document.getElementById("btn");
+    var getContents = document.getElementById("content");
+    if (getBodiesCurrentClass == "light-mode")
+        {
+            docBody.className = "dark-mode";
+            getContents.style.color= '#d8dbe3';
+            buttonvar.innerHTML = "Light Mode";
+            buttonvar.className = "btn text-center btn-light";
+            getContents.classList += "dark-mode mt-5";
+        }
+    else
+        {
+            getContents.classList += "light-mode mt-5";
+            getContents.style.color = '#111';
+            docBody.className = "light-mode";
+            buttonvar.innerHTML = "Dark Mode";
+            buttonvar.className = "btn text-center btn-dark";
+        }
+}
